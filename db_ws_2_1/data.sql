e9018a2abaf5c78da719f99e48b45a5b65fa925c
kimkyungjin <1010kkj@naver.com>
2025-04-08 16:57:32 +0900

    0408 실습1단계

---
+++ db_ws_2_1/data.sql
@@ -0,0 +1,21 @@
+SELECT * from zoo;
+
+DROP TABLE zoo;
+
+CREATE TABLE zoo (
+    name TEXT,
+    eat TEXT,
+    weight INTEGER,
+    height INTEGER,
+    age INTEGER
+);
+
+
+INSERT INTO zoo (name, eat, weight, height, age)
+VALUES
+    ('Lion', 'Meat', 200, 120, 5),
+    ('Elephant', 'Plants', 5000, 300, 15),
+    ('Giraffe', 'Leaves', 1500, 550, 10),
+    ('Monkey', 'Fruits', 50, 60, 8);
+
+    
